30b786e56b531381b37de8b438b8e27f1a365053
PokerBeing <pokerbeing@yahoo.com>
2022-08-25 12:16:07 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./styles.css";
+
+export default class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+
+  incrementCount() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  decrementCount() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    return (
+      <div className="Counter">
+        <h1>Counter</h1>
+        <label className="number">{this.state.count}</label>
+        <br></br>
+        <button id="up" onClick={() => this.incrementCount()}>
+          Increment
+        </button>
+        <button id="down" onClick={() => this.decrementCount()}>
+          Increment
+        </button>
+      </div>
+    );
+  }
+}
