b3c142fda64eb7ae7bb9f9e5b83c6f6f3ad11d45
PokerBeing <pokerbeing@yahoo.com>
2022-08-25 18:37:53 +0000

    Added random color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./styles.css";
 
+// Create a new component class called Counter
 export default class Counter extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    // Add a state that is a dictionary containing the key count
     this.state = {
-      count: 0
+      count: 0,       // initialize count to 0
+      color: "blue",
     };
   }
 
@@ -17,17 +20,27 @@ export default class Counter extends React.Component {
     this.setState({ count: this.state.count - 1 });
   }
 
+  updateColor(){
+    const colors = ['red', 'orange', 'blue', 'green', 'teal']
+    const color = colors[Math.floor(Math.random() * colors.length)];
+
+    this.setState({ color: color});
+  }
+  
   render() {
     return (
       <div className="Counter">
-        <h1>Counter</h1>
+        <h1 style={{color: this.state.color}}  >Counter</h1>
         <label className="number">{this.state.count}</label>
         <br></br>
         <button className="button btn-min" id="up" onClick={() => this.incrementCount()}>
           Increment
         </button>
         <button id="down" onClick={() => this.decrementCount()}>
-          Increment
+          Decrement
+        </button > 
+        <button onClick={()=>this.updateColor()}>
+        Change color
         </button>
       </div>
     );
